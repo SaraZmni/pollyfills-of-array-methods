f6b6960aa97e92d7a5500875bf0716538e92fb3f
Emma <Emma@fanap.ir>
2024-09-08 13:14:31 +0330

    complete filter method

--- dist/filter.js
+++ dist/filter.js
@@ -5,9 +5,13 @@ const filter = (list, callback) => {
     const result = [];
     for (let index = 0; index < list.length; index++) {
         const hasPassed = callback(list[index], index, list);
-        console.log(hasPassed);
+        if (hasPassed) {
+            result.push(list[index]);
+        }
     }
+    return result;
 };
 exports.filter = filter;
-(0, exports.filter)([1, 2, 3, 4, 5], (item) => item < 3);
+const result = (0, exports.filter)([1, 2, 3, 4, 5], (item) => item < 3);
+console.log('result', result);
 //# sourceMappingURL=filter.js.map
